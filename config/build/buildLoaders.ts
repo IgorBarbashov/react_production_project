1f39f1a39703d595b8f4b33ec525b73562e97af6
Igor Barbashov <bis200705@gmail.com>
2024-03-10 13:39:58 +0300

    feat: extract css into separate files

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,4 +1,5 @@
 import { RuleSetRule } from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildLoaders(): RuleSetRule [] {
 
@@ -6,7 +7,7 @@ export function buildLoaders(): RuleSetRule [] {
         test: /\.s[ac]ss$/i,
         use: [
             // Creates `style` nodes from JS strings
-            "style-loader",
+            MiniCssExtractPlugin.loader,
             // Translates CSS into CommonJS
             "css-loader",
             // Compiles Sass to CSS
