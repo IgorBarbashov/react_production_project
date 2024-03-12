b7bc9b71e82a9dbee6a5be1474c5d89f83fae6f3
Igor Barbashov <bis200705@gmail.com>
2024-03-12 22:59:09 +0300

    feat: add themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { MainPage } from "./pages/MainPage";
 import { AboutPage } from "./pages/AboutPage";
 import './styles/index.scss';
 
+export enum THEME {
+    LIGHT = 'light',
+    DARK = 'dark',
+}
+
 export const App = () => {
+    const [theme, setTheme] = useState<THEME>(THEME.LIGHT);
+
+    const toggleTheme = () => setTheme(theme === THEME.LIGHT ? THEME.DARK : THEME.LIGHT);
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to='/'>MainPage</Link>
             <Link to='/about'>AboutPage</Link>
             <Suspense fallback={'Loading...'}>
