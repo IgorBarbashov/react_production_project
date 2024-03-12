d7528d284398adcdc5f31dd5eead338ee20ca23d
Igor Barbashov <bis200705@gmail.com>
2024-03-12 22:11:03 +0300

    feat: check fallback inb Suspense component (only for education example)

--- src/pages/AboutPage/AboutPageLazy.tsx
+++ src/pages/AboutPage/AboutPageLazy.tsx
@@ -1,3 +1,9 @@
 import { lazy } from "react";
 
-export const AboutPageLazy = lazy(() => import('./AboutPage'));
+export const AboutPageLazy = lazy(() => new Promise(resolve => {
+    setTimeout(() => {
+        // @ts-ignore
+        // only for education example
+        resolve(import('./AboutPage'));
+    }, 1500);
+}));
--- src/pages/MainPage/MainPageLazy.tsx
+++ src/pages/MainPage/MainPageLazy.tsx
@@ -1,3 +1,9 @@
 import { lazy } from "react";
 
-export const MainPageLazy = lazy(() => import('./MainPage'));
+export const MainPageLazy = lazy(() => new Promise(resolve => {
+    setTimeout(() => {
+        // @ts-ignore
+        // only for education example
+        resolve(import('./MainPage'));
+    }, 1500);
+}));
