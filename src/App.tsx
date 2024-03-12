262dbf4e648a94d94275689a074e35cbfb04d50c
Igor Barbashov <bis200705@gmail.com>
2024-03-12 21:36:19 +0300

    feat: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Counter } from "./Components/Counter";
+import { Routes, Route, Link } from "react-router-dom";
+import { MainPage } from "./pages/MainPage/MainPage";
+import { AboutPage } from "./pages/AboutPage/AboutPage";
 import './index.scss';
 
 export const App = () => {
     return (
         <div className='app'>
-            dsflkjgdflkgjsldkgf
-            <Counter/>
+            <Link to='/'>MainPage</Link>
+            <Link to='/about'>AboutPage</Link>
+            <Routes>
+                <Route path='/' element={<MainPage />}/>
+                <Route path='/about' element={<AboutPage />}/>
+            </Routes>
         </div>
     );
 };
