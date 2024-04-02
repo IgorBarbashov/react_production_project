e280e29650e98d9a4d1ee8866214cc96a2c1226f
Igor Barbashov <bis200705@gmail.com>
2024-04-02 15:50:29 +0300

    feat: add UI-kit component Button (according FSD 'shared/ui' layer, Public API, LIP from SOLID)

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,5 @@
 import {FC} from "react";
-import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+import {ThemeSwitcher} from "features/ThemeSwitcher";
 import {AppLink} from "shared/ui";
 import {classNames} from "shared/lib";
 import {RoutesPath} from "shared/router";
