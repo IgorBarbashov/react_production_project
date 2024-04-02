cc0360b85ccf34d1e89d116c795788a2a50a8a92
Igor Barbashov <bis200705@gmail.com>
2024-04-02 12:23:32 +0300

    feat: add styles for Navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import {FC} from "react";
 import {Link} from "react-router-dom";
 import {classNames} from "shared/lib";
+import {RoutesPath} from "shared/router";
 import cls from './Navbar.module.scss';
 
 interface INavbarProps {
@@ -10,8 +11,10 @@ interface INavbarProps {
 export const Navbar: FC<INavbarProps> = ({className}) => {
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <Link to='/'>MainPage</Link>
-            <Link to='/about'>AboutPage</Link>
+            <div className={cls.links}>
+                <Link to={RoutesPath.main} className={cls.mainLink}>MainPage</Link>
+                <Link to={RoutesPath.about}>AboutPage</Link>
+            </div>
         </div>
     );
 };
