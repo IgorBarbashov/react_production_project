1b4e9e1f5d3966aba0beb70f880a71b2bc875d08
Igor Barbashov <bis200705@gmail.com>
2024-04-03 20:40:04 +0300

    feature: add unit-test for 'classNames' helper

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -15,9 +15,9 @@ export function classNames(cls: string, mods?: Mods | string[], additional?: str
 
     return [
         cls,
+        ...(additional || []).filter(Boolean),
         ...Object.entries(mods)
             .filter(([, value]) => Boolean(value))
             .map(([key]) => key),
-        ...(additional || []).filter(Boolean),
     ].join(' ');
 }
